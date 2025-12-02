1f9ce3fe747232f87bf370f4055a0cae74c91c4c
capkimkhanh2k5 <capkimkhanh2k5@gmail.com>
2025-12-02 11:11:35 +0700

    update: template practice 5

---
+++ OnCK/Practice/Bai5/TCP/server.java
@@ -0,0 +1,64 @@
+package OnCK.Practice.Bai5.TCP;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5001);
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            System.out.println("Server is running on port 5001...");
+
+            while (true) {
+                Socket soc = serverSocket.accept();
+                System.out.println("Client connected: " + soc.getInetAddress().getHostAddress() + ":" + soc.getPort());
+
+                serverHandle handle = new serverHandle(soc);
+                executorService.execute(handle);
+            }
+        } catch (Exception e) {
+            System.out.println("Error in server!");
+            e.printStackTrace();
+        }
+    }
+}
+
+class serverHandle extends Thread {
+    private Socket soc;
+
+    public serverHandle(Socket soc) {
+        this.soc = soc;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream dis = new DataInputStream(soc.getInputStream());
+            DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+
+            // TODO: Nhận dữ liệu từ client
+            String receivedData = dis.readUTF();
+            System.out.println("Server received: " + receivedData);
+
+            // TODO: Xử lý dữ liệu và gửi response
+            String response = "Response from server";
+            dos.writeUTF(response);
+            dos.flush();
+            System.out.println("Server sent: " + response);
+
+            // Đóng kết nối
+            dis.close();
+            dos.close();
+            soc.close();
+        } catch (IOException e) {
+            System.out.println("Error in serverHandle!");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ OnCK/Practice/Bai5/UDP/server.java
@@ -0,0 +1,72 @@
+package OnCK.Practice.Bai5.UDP;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class server {
+    public static void main(String[] args) {
+        new server();
+    }
+
+    public server() {
+        try {
+            System.out.println("Server is running in port 5000...");
+            DatagramSocket socket = new DatagramSocket(5000);
+
+            ExecutorService executorService = Executors.newCachedThreadPool();
+
+            while (true) {
+                byte[] buffer = new byte[1024];
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                serverHandle handle = new serverHandle(socket, packet);
+
+                executorService.execute(handle);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error in server!");
+            e.printStackTrace();
+        }
+    }
+}
+
+class serverHandle extends Thread {
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+
+    public serverHandle(DatagramSocket socket, DatagramPacket packet) {
+        this.socket = socket;
+        this.packet = packet;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Nhận dữ liệu từ client
+            String receivedData = new String(packet.getData(), 0, packet.getLength()).trim();
+            System.out.println("Server received: " + receivedData);
+
+            // TODO: Xử lý dữ liệu
+            String response = "Response from server";
+
+            // Gửi response về client
+            byte[] buffer = response.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(
+                    buffer,
+                    buffer.length,
+                    packet.getAddress(),
+                    packet.getPort());
+            socket.send(sendPacket);
+
+            System.out.println("Server sent: " + response);
+        } catch (Exception e) {
+            System.out.println("Error in serverHandle!");
+            e.printStackTrace();
+        }
+    }
+}
